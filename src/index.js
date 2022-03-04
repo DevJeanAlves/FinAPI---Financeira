1823f4bff27a3c48a8815c2a49acdaeea4b15ac4
JeanAlves2021 <jean.pdolsky2@gmail.com>
2022-03-03 22:44:15 -0300

    commit avanço nos requisitos e regras de negocio.

--- src/index.js
+++ src/index.js
@@ -34,4 +34,18 @@ app.post('/account', (request, response) => {
   return response.status(201).send()
 })
 
+app.get('/statement/:cpf', (request, response) => {
+  const { cpf } = request.params
+
+  const custumer = custumers.find(custumer => custumer.cpf === cpf)
+
+  if (!custumer) {
+    return response.status(400).json({ error: 'Custumer not found.' })
+  }
+
+  return response.json(custumer.statement)
+})
+
 app.listen(3333)
+
+console.log('rodou liso')
