7976420794da4456888a5760753b892e524202a9
JeanAlves2021 <jean.pdolsky2@gmail.com>
2022-03-08 00:18:49 -0300

    Update index.js
    I create the deposit's route, now is possible show the statement deposit stats.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const { v4: uuidv4 } = require('uuid')
 const app = express()
 app.use(express.json())
 
-const custumers = []
+const customers = []
 /*
 cpf - string
 name - string
@@ -12,20 +12,35 @@ id - uuid
 statement - array
 */
 
+//middleware
+function verifyIfExistsAccountCpf(request, response, next) {
+  const { cpf } = request.headers //request.params
+
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+  if (!customer) {
+    return response.status(401).json({ error: 'Customer not found.' })
+  }
+
+  request.customer = customer
+
+  return next()
+}
+
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body
 
-  const custumersAlreadyExists = custumers.some(
-    custumers => custumers.cpf === cpf
+  const customersAlreadyExists = customers.some(
+    customers => customers.cpf === cpf
   )
 
-  if (custumersAlreadyExists) {
+  if (customersAlreadyExists) {
     return response
       .status(400)
       .json({ error: 'Cpf already registed (custumer already exists)' })
   }
 
-  custumers.push({
+  customers.push({
     cpf,
     name,
     id: uuidv4(),
@@ -33,17 +48,27 @@ app.post('/account', (request, response) => {
   })
   return response.status(201).send()
 })
+// ====='/statement/:cpf'
+app.get('/statement', verifyIfExistsAccountCpf, (request, response) => {
+  const { customer } = request
+  return response.status(200).json(customer.statement)
+})
 
-app.get('/statement/:cpf', (request, response) => {
-  const { cpf } = request.params
+app.post('/deposit', verifyIfExistsAccountCpf, (request, response) => {
+  const { description, amount } = request.body
 
-  const custumer = custumers.find(custumer => custumer.cpf === cpf)
+  const { customer } = request
 
-  if (!custumer) {
-    return response.status(400).json({ error: 'Custumer not found.' })
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: 'credito'
   }
 
-  return response.json(custumer.statement)
+  customer.statement.push(statementOperation)
+
+  return response.status(201).send()
 })
 
 app.listen(3333)
