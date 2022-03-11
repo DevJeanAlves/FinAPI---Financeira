02b150ea20df45101a11bb9f6531c08a88502dfa
JeanAlves2021 <jean.pdolsky2@gmail.com>
2022-03-10 22:01:38 -0300

    Search statement system

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { json } = require('express/lib/response')
 const { v4: uuidv4 } = require('uuid')
 
 const app = express()
@@ -102,6 +103,21 @@ app.post('/withdraw', verifyIfExistsAccountCpf, (request, response) => {
   customer.statement.push(statementOperation)
 
   return response.status(200).send()
+})
+
+app.get('/statement/date', verifyIfExistsAccountCpf, (request, response) => {
+  const { date } = request.query // 2022-03-09
+  const { customer } = request
+
+  const dateFormat = new Date(date + ' 00:00')
+
+  const statement = customer.statement.filter(
+    statement =>
+      statement.created_at.toDateString() ===
+      new Date(dateFormat).toDateString()
+  )
+
+  return response.json(statement)
 })
 
 app.listen(3333)
